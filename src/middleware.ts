4bbf54a57b0d09c75c7f53ce4d3a3601f743285a
Lewis Barnes <lewis_barnes@live.co.uk>
2022-09-03 18:17:17 +0100

    main functionality complete

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+	const slug = request.nextUrl.pathname.split('/').pop();
+
+	const shortLink = await fetch(`${request.nextUrl.origin}/api/url/${slug}`);
+
+	if(shortLink.status === 404) {
+		return NextResponse.redirect(request.nextUrl.origin);
+	}
+	const data = await shortLink.json();
+
+	if(data?.url) {
+		return NextResponse.redirect(data.url);
+	}
+}
+
+export const config = {
+	matcher: "/s/:slug*",
+}
