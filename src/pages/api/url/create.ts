354157c7f44d3e119c81cbea3adf8f1083990ffe
Lewis Barnes <lewis_barnes@live.co.uk>
2022-09-19 13:07:59 +0100

    changed api endpoints to use new url schema

--- src/pages/api/url/create.ts
+++ src/pages/api/url/create.ts
@@ -1,36 +1,34 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../../server/db/client";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../../server/db/client';
 
 export const CreateHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-	if(req.method === 'POST') {
-		const { slug, url } = JSON.parse(req.body);
-		console.log(slug);
-		const slugMatch = await prisma.shortlink.count({
-			where: {
-				slug: {
-					equals: slug,
-				},
-			},
-		})
-		if(slugMatch > 0) {
-			res.statusCode = 400;
-			res.send(JSON.stringify({message: 'slug already in use'}));
-			return;
-		}
-		const data = await prisma.shortlink.create({
-			data: {
-				slug: slug,
-				url: url,
-			},
-			select: {slug: true},
-		});
-		res.statusCode = 201;
-		return res.json(data);
-	} else {
-		res.statusCode = 405;
-		res.send(JSON.stringify({message: 'only POST requests are allowed on this endpoint'}));
-	}
-
+  if (req.method === 'POST') {
+    res.statusCode = 405;
+    res.send(JSON.stringify({ message: 'only POST requests are allowed on this endpoint' }));
+  }
+  const { slug, url } = JSON.parse(req.body);
+  console.log(slug);
+  const slugMatch = await prisma.shortlink.count({
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
+  if (slugMatch > 0) {
+    res.statusCode = 400;
+    res.send(JSON.stringify({ message: 'slug already in use' }));
+    return;
+  }
+  const data = await prisma.shortlink.create({
+    data: {
+      slug: slug,
+      url: url,
+    },
+    select: { slug: true },
+  });
+  res.statusCode = 201;
+  return res.json(data);
 };
 
 export default CreateHandler;
--- src/pages/api/url/shorten.ts
+++ src/pages/api/url/shorten.ts
@@ -1,43 +1,42 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../../server/db/client";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../../server/db/client';
 import { nanoid } from 'nanoid';
 
 export const ShortenHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-	if(req.method === 'POST') {
-		const { url } = req.body;
-		if(typeof url !== 'string' || url.length === 0) {
-			res.statusCode = 400;
-			return res.send(JSON.stringify({message: 'url not provided in request body'}));
-		}
-		let slug = nanoid(10);
-		let hasMatch =  await slugMatch(slug);
-		while(hasMatch > 0) {
-			slug = nanoid(10);
-			hasMatch =  await slugMatch(slug);
-		}
-		const data = await prisma.shortlink.create({
-			data: {
-				slug: slug,
-				url: url,
-			},
-			select: {slug: true},
-		});
-		res.statusCode = 201;
-		return res.json({shortLink: `https://${req.headers.host}/s/${data.slug}`});
-	} else {
-		res.statusCode = 405;
-		res.send(JSON.stringify({message: 'only POST requests are allowed on this endpoint'}));
-	}
+  if (req.method !== 'POST') {
+    res.statusCode = 405;
+    res.send(JSON.stringify({ message: 'only POST requests are allowed on this endpoint' }));
+  }
+  const { url } = req.body;
+  if (typeof url !== 'string' || url.length === 0) {
+    res.statusCode = 400;
+    return res.send(JSON.stringify({ message: 'url not provided in request body' }));
+  }
+  let slug = nanoid(10);
+  let hasMatch = await slugMatch(slug);
+  while (hasMatch > 0) {
+    slug = nanoid(10);
+    hasMatch = await slugMatch(slug);
+  }
+  const data = await prisma.shortlink.create({
+    data: {
+      slug: slug,
+      url: url,
+    },
+    select: { slug: true },
+  });
+  res.statusCode = 201;
+  return res.json({ shortLink: `https://${req.headers.host}/${data.slug}` });
 };
 
-const slugMatch = async(slug: string) => {
-	return await prisma.shortlink.count({
-		where: {
-			slug: {
-				equals: slug,
-			},
-		},
-	});
-}
+const slugMatch = async (slug: string) => {
+  return await prisma.shortlink.count({
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
+};
 
 export default ShortenHandler;
